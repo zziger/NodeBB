59db634ee4f6661ea240fa134b48b779dc9328f7
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-02-16 08:59:31 -0500

    feat: remove unused code

--- src/translator.js
+++ src/translator.js
@@ -7,20 +7,4 @@ const warn = function (msg) {
 	winston.warn(msg);
 };
 
-// async function loadServer(language, namespace) {
-// 	return await languages.get(language, namespace);
-// }
-
-// function loadServer(language, namespace) {
-// 	return new Promise(function (resolve, reject) {
-// 		languages.get(language, namespace, function (err, data) {
-// 			if (err) {
-// 				reject(err);
-// 			} else {
-// 				resolve(data);
-// 			}
-// 		});
-// 	});
-// }
-
 module.exports = require('../public/src/modules/translator.core')(require('./utils'), languages.get, warn);
