660c35b8f59993c998c8e945e9ed9f6deec65c17
Barış Soner Uşaklı <barisusakli@gmail.com>
2020-02-23 12:01:18 -0500

    fix: jquery-ui require

--- public/src/modules/settings/sorted-list.js
+++ public/src/modules/settings/sorted-list.js
@@ -1,5 +1,5 @@
 
-define('settings/sorted-list', ['benchpress', 'jqueryui'], function (benchpress) {
+define('settings/sorted-list', ['benchpress', 'jquery-ui'], function (benchpress) {
 	var SortedList;
 	var Settings;
 
